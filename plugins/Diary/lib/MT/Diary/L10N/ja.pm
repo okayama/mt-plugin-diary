d37d246b80153294491787926a50789c3063c760
okayama <kenmin.okayama@gmail.com>
2011-04-20 15:15:09 +0900

    first commit

---
+++ plugins/Diary/lib/MT/Diary/L10N/ja.pm
@@ -0,0 +1,11 @@
+package MT::Diary::L10N::ja;
+use strict;
+use base qw/ MT::Diary::L10N MT::L10N MT::Plugin::L10N /;
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Utility for creating entry for diary.' => '日記エントリーを作成するためのサポートツールです。',
+    'Active' => '有効',
+);
+
+1;
---
+++ plugins/Diary/lib/MT/Diary/Plugin.pm
@@ -0,0 +1,32 @@
+package MT::Diary::Plugin;
+use strict;
+
+use MT::Util qw( offset_time_list format_ts );
+
+my $plugin = MT->component( 'Diary' );
+
+sub _cb_tp_edit_entry {
+    my ( $cb, $app, $param, $tmpl ) = @_;
+    if ( my $blog = $app->blog ) {
+        my $blog_id = $blog->id;
+        my $scope = 'blog:' . $blog_id;
+        return unless $plugin->get_config_value( 'is_active', 'blog:' . $blog_id );
+        if ( ! $app->param( 'id' ) && ! $app->param( 'reedit' ) ) {
+            if ( $app->param( 'title' ) ) {
+                return 1;
+            }
+            my @tl = offset_time_list( time, $blog );
+            my $title = sprintf "%04d-%02d-%02d", $tl[ 5 ] + 1900, $tl[ 4 ] + 1, @tl[ 3, 2, 1, 0 ];
+            my $redirect_url = $app->base . $app->uri( mode => 'view',
+                                                       args => {
+                                                           _type => $app->param( '_type' ),
+                                                           blog_id => $blog_id,
+                                                           title => $title,
+                                                       },
+                                                     );
+            return $app->redirect( $redirect_url );
+        }
+    }
+}
+
+1;
